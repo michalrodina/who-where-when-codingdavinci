8a7567bc6c7554cb37a122788d79921daff2cbf8
rcl <michal.rodina@gmail.com>
2022-03-22 10:33:42 +0100

    OpenStreetMap minimal example

--- main.py
+++ main.py
@@ -4,25 +4,53 @@
 # # TODO:
 # - WSGI integrace pro produkci
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from dataapi import data_api
+import json
 
 # Inicializace Flask aplikace
 app = Flask(__name__, template_folder='./templates/')
 app.register_blueprint(data_api)
 
+config_json = open('conf/sites.json')
+configs = json.load(config_json)
+
+
+##
+# JS router
+#
+# poskytuje soubory ze slozky js
+@app.route('/js/<path:path>')
+def file_js(path):
+    return send_from_directory('js', path)
+
+
+##
+# CSS router
+#
+# poskytuje soubory ze slozky styles
+@app.route('/styles/<path:path>')
+def file_css(path):
+    return send_from_directory('styles', path)
 
 ##
 # Index router
 #
 # Vychozi stranka aplikace
-@app.route('/')
-@app.route('/index.html')
-def index():
+@app.route('/', defaults={'config': 'general'})
+@app.route('/<config>')
 
-    return render_template('index.html.tpl')
+def index(config):
+    if config not in configs.keys():
+        config = 'general'
+
+    center_lat = configs[config]['map']['center_lat']
+    center_lon = configs[config]['map']['center_lon']
+    zoom = configs[config]['map']['zoom']
+
+    return render_template('index.html.tpl', center_lat=center_lat, center_lon=center_lon, zoom=zoom)
     pass
 
 
-# Spusteni VYVOJOVEHO server
+# Spusteni VYVOJOVEHO serveru
 app.run(host='127.0.0.1', port=8080, debug=True, ssl_context='adhoc')
--- templates/index.html.tpl
+++ templates/index.html.tpl
@@ -3,13 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <style type="text/css">
-
-    </style>
+    <script type="text/javascript" src="js/OpenLayers/OpenLayers.js"></script>
+    <link type="text/css" rel="stylesheet" href="styles/main.css" />
 </head>
 <body>
+<div id="header"></div>
 <div id="map-container">
-
+<div id="map"></div>
 </div>
+<div id="footer"></div>
+<script type="text/javascript">
+map = new OpenLayers.Map("map");
+var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+
+map.addLayer(new OpenLayers.Layer.OSM());
+map.setCenter(new OpenLayers.LonLat({{center_lon}}, {{center_lat}}).transform( fromProjection, toProjection), {{zoom}})
+</script>
 </body>
 </html>
